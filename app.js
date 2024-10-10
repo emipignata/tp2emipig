f34334167f713619633d04926f5b41fde2858ae2
Pablo Fernandez Hinojosa <pablo.hinojosa@raona.com>
2024-10-09 23:20:54 -0300

    examen 2024 c2

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 import "dotenv/config";
 import express from "express";
-import moviesRouter from "./routes/movies.js";
 import salesRouter from "./routes/sales.js";
 
 const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
 app.use("/api/sales", salesRouter);
-app.use("/api/movies", moviesRouter);
 
 app.listen(PORT, () => {
   console.log("Servidor Web en el puerto:", PORT);
